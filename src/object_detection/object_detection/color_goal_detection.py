6dd6eb4a0f3a2315fc2c517232c31ef1a0dccb61
Jaedon-A <jsballeyne@optimum.net>
2026-04-03 20:12:41 -0400

    more

--- src/object_detection/object_detection/color_goal_detection.py
+++ src/object_detection/object_detection/color_goal_detection.py
@@ -61,7 +61,7 @@ class ColorObjDetectionNode(Node):
         
         # Declare the parameters for the color detection
         self.declare_parameter('color_low', [200, 25, 43])
-        self.declare_parameter('color_high', [226, 100, 63])
+        self.declare_parameter('color_high', [226, 100, 100])
         self.declare_parameter('object_size_min', 1000)
         # Used to convert between ROS and OpenCV images
         self.br = CvBridge()
