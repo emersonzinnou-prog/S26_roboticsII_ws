db8705a3026c5c3ab36cd70e41b37960e1cdb061
Jaedon-A <jsballeyne@optimum.net>
2026-04-08 01:19:00 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -299,7 +299,7 @@ class TrackingNode(Node):
             #world_obs_pose = self.robot_world_R@self.obs_pose+np.array([self.robot_world_x,self.robot_world_y,self.robot_world_z])
             world_obs_pose = obs_pose
             print("obs:", world_obs_pose)
-            dis_obj = world_obs_pose - pose + 0.3
+            dis_obj = world_obs_pose - pose + 0.1
             if np.sqrt(dis_obj) < Q:
                 U_grad = U_grad + 0.5*n*(1/Q - 1/np.linalg.norm(dis_obj))*1/np.linalg.norm(dis_obj)**2*(dis_obj/np.linalg.norm(dis_obj))
         
