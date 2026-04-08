43049491b1cf50926bae8061e60a3e56ba8ecd19
Jaedon-A <jsballeyne@optimum.net>
2026-04-08 02:23:29 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -260,7 +260,7 @@ class TrackingNode(Node):
 
         #new code:
         Kp = 2
-        Kt = 0.1
+        Kt = 0.5
         zetta = 1
         n = 0.5
         Q = 0.6
@@ -276,7 +276,7 @@ class TrackingNode(Node):
         if self.state == "Home":
             world_goal_pose = self.start
 
-        dis_goal = (world_goal_pose - pose)
+        dis_goal = (pose - world_goal_pose)
 
         if np.sqrt((dis_goal[0])**2 + (dis_goal[1])**2) < 0.3:
             print("close to goal")
@@ -312,7 +312,7 @@ class TrackingNode(Node):
         cmd_vel.linear.x = max(-0.05,min(0.05, Kp*U_grad[0]/np.linalg.norm(U_grad[0:2])))
         cmd_vel.linear.y = max(-0.15,min(0.15, 0.6*U_grad[1]/np.linalg.norm(U_grad[0:2])))
         #cmd_vel.linear.y = 0
-        #cmd_vel.angular.z = max(-2, min(2, Kt*theta))
+        #cmd_vel.angular.z = max(-2, min(2, -Kt*theta))*0.01
         
         
         return cmd_vel
