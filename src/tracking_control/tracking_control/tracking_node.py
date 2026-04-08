e6bdb84e78102f8327e1e31be12a4abd0f9c3e5a
Jaedon-A <jsballeyne@optimum.net>
2026-04-08 01:36:06 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -310,7 +310,7 @@ class TrackingNode(Node):
         theta = np.arctan2(dis_goal[1], dis_goal[0])
         cmd_vel = Twist()
         cmd_vel.linear.x = max(-0.05,min(0.05, Kp*U_grad[0]/np.linalg.norm(U_grad[0:2])))
-        cmd_vel.linear.y = max(-0.15,min(0.15, 0.7*U_grad[1]/np.linalg.norm(U_grad[0:2])))
+        cmd_vel.linear.y = max(-0.15,min(0.15, Kp**U_grad[1]/np.linalg.norm(U_grad[0:2])))
         #cmd_vel.linear.y = 0
         #cmd_vel.angular.z = max(-2, min(2, -Kt*theta))*0.01
         
