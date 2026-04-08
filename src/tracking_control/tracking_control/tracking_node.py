59b866236084956f771ee279be49d284754a73d3
Jaedon-A <jsballeyne@optimum.net>
2026-04-08 00:49:59 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -262,7 +262,7 @@ class TrackingNode(Node):
         Kp = 2
         Kt = 0.5
         zetta = 1
-        n = 1
+        n = 2
         Q = 0.3
 
         pose = self.robot_world_R@np.array([-self.robot_world_x, -self.robot_world_y, self.robot_world_z])
