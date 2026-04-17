a09924e8770a85271ec6d429bd16ea485812908b
emersonzinnou-prog <emersonzinnou@gmail.com>
2026-04-16 20:03:03 -0400

    Update em_test.py

--- src/tracking_control/tracking_control/em_test.py
+++ src/tracking_control/tracking_control/em_test.py
@@ -166,7 +166,7 @@ class TrackingNode(Node):
         heading_error = math.atan2(y, x)
         distance = math.sqrt(x**2 + y**2)
 
-        stop_distance = 0.4
+        stop_distance = 0.3
         k_linear = 0.6
         k_angular = 1.7
 
