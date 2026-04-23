e52591b37a97d77ebb48d8aec1bf60c853150805
emersonzinnou-prog <emersonzinnou@gmail.com>
2026-04-23 15:15:46 -0400

    Update em_test.py

--- src/tracking_control/tracking_control/em_test.py
+++ src/tracking_control/tracking_control/em_test.py
@@ -448,11 +448,11 @@ class TrackingNode(Node):
         
         print("heading error:", heading_error)
         
-        gamma_star = max(-0.3, min(0.3, K_h * heading_error))
+        gamma_star = max(-0.1, min(0.1, K_h * heading_error))
         
         dist_goal = np.sqrt(dis_goal[0]**2 + dis_goal[1]**2)
         
-        if dist_goal < 0.20:
+        if dist_goal < 0.30:
             K_v_use = 0.15
         else:
             K_v_use = K_v
